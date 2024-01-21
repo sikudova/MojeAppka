8a69942695e5916e17a36add2f832c65e3715bf7
Lucie Šikudová <lsikudova@utb.cz>
2024-01-21 17:59:01 +0100

    WIP: icons (bookmark and home)

--- app/src/main/java/com/example/mojeappka/presentation/news_navigator/NewsNavigator.kt
+++ app/src/main/java/com/example/mojeappka/presentation/news_navigator/NewsNavigator.kt
@@ -38,9 +38,9 @@ fun NewsNavigator() {
 
     val bottomNavigationItems = remember {
         listOf(
-//            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
+            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
             BottomNavigationItem(icon = R.drawable.ic_search, text = "Search"),
-//            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark"),
+            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark"),
         )
     }
 
--- app/src/main/java/com/example/mojeappka/presentation/news_navigator/components/NewsBottomNavigation.kt
+++ app/src/main/java/com/example/mojeappka/presentation/news_navigator/components/NewsBottomNavigation.kt
@@ -74,9 +74,9 @@ data class BottomNavigationItem(
 fun NewsBottomNavigationPreview() {
     MojeAppkaTheme(dynamicColor = false) {
         NewsBottomNavigation(items = listOf(
-//            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
+            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
             BottomNavigationItem(icon = R.drawable.ic_search, text = "Search"),
-//            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark"),
+            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark"),
         ), selectedItem = 0, onItemClick = {})
     }
 }
