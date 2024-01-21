30da814afb1adfa74a0bc0cbaa5b3f885ce309b8
Lucie Šikudová <lsikudova@utb.cz>
2024-01-21 19:33:55 +0100

    WIP: local data

---
+++ app/src/main/java/com/example/mojeappka/data/local/NewsDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.mojeappka.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.mojeappka.domain.model.Article
+
+@Database(entities = [Article::class], version = 1)
+@TypeConverters(NewsTypeConverter::class)
+abstract class NewsDatabase : RoomDatabase() {
+
+    abstract val newsDao: NewsDao
+
+}
