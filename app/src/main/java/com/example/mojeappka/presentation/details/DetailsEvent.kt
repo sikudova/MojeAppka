26295c1b0942920e5bdd312d4361a57f0c26aa3f
Lucie Šikudová <lsikudova@utb.cz>
2024-01-22 09:47:34 +0100

    WIP: details page

--- app/src/main/java/com/example/mojeappka/presentation/details/DetailsEvent.kt
+++ app/src/main/java/com/example/mojeappka/presentation/details/DetailsEvent.kt
@@ -1,7 +1,10 @@
 package com.example.mojeappka.presentation.details
 
+import com.example.mojeappka.domain.model.Article
+
 sealed class DetailsEvent {
 
-    object SaveArticle : DetailsEvent()
+    data class UpsertDeleteArticle(val article: Article) : DetailsEvent()
 
+    object RemoveSideEffect : DetailsEvent()
 }
