6f2219425415fa65fb3aeb19385a32d56de2a998
Lucie Šikudová <lsikudova@utb.cz>
2024-01-22 16:52:25 +0100

    WIP: small padding

--- app/src/main/java/com/example/mojeappka/presentation/Dimension.kt
+++ app/src/main/java/com/example/mojeappka/presentation/Dimension.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.unit.dp
 object Dimension {
     val ExtraSmallPadding = 3.dp
     val ExtraSmallPadding2 = 6.dp
+    val SmallPadding = 15.dp
     val MediumPadding1 = 24.dp
     val MediumPadding2 = 30.dp
     val MediumPadding3 = 40.dp
