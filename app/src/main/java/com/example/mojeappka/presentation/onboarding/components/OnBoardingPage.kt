d931d03283e0328f22a860a813a5559df5e1a91c
Lucie Šikudová <lsikudova@utb.cz>
2024-01-22 16:55:55 +0100

    WIP: spacer

--- app/src/main/java/com/example/mojeappka/presentation/onboarding/components/OnBoardingPage.kt
+++ app/src/main/java/com/example/mojeappka/presentation/onboarding/components/OnBoardingPage.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mojeappka.presentation.Dimension.MediumPadding1
 import com.example.mojeappka.presentation.Dimension.MediumPadding2
 import com.example.mojeappka.presentation.onboarding.Page
@@ -30,6 +31,7 @@ fun OnBoardingPage(
     page: Page
 ) {
     Column(modifier = modifier) {
+        Spacer(modifier = Modifier.height(50.dp))
         Image(
             modifier = Modifier
                 .fillMaxWidth()
